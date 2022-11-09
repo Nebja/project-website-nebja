c2318f77f2e1cf994c9b8b9a4ee3475112ad7b3b
benja <benjaminkotsaridis@outlook.com.gr>
2022-11-09 19:28:06 +0100

    Home Videos (added files)

---
+++ src/Entity/Home.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HomeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=HomeRepository::class)
+ */
+class Home
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $comments;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $permissions = [];
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $file;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getComments(): ?string
+    {
+        return $this->comments;
+    }
+
+    public function setComments(string $comments): self
+    {
+        $this->comments = $comments;
+
+        return $this;
+    }
+
+    public function getPermissions(): array
+    {
+        $roles = $this->permissions;
+        $roles[] ='ROLE_USER';
+        return array_unique($roles);
+    }
+
+    public function setPermissions(array $permissions): self
+    {
+        $this->permissions = $permissions;
+
+        return $this;
+    }
+
+    public function getFile(): ?string
+    {
+        return $this->file;
+    }
+
+    public function setFile(string $file): self
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+}
---
+++ src/Repository/HomeRepository.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Home;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Home>
+ *
+ * @method Home|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Home|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Home[]    findAll()
+ * @method Home[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class HomeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Home::class);
+    }
+
+    public function add(Home $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Home $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    /**
+     * @param $value
+     * @return float|int|array|string
+     */
+    public function findByFile($value): float|int|array|string
+    {
+        return $this->createQueryBuilder('h')
+            ->where('h.file = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getArrayResult();
+    }
+
+//    /**
+//     * @return Home[] Returns an array of Home objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('h')
+//            ->andWhere('h.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('h.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Home
+//    {
+//        return $this->createQueryBuilder('h')
+//            ->andWhere('h.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
